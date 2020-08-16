4e2cf4a37c9d379d29e74893605e3d532c047269
Akib Al-Amin <akib.swe@gmail.com>
2020-08-16 18:52:19 +0600

    curl update

--- 23_Curl/3_Https_request_process_&_output_SSL_certificateIssue_fixed.php
+++ 23_Curl/3_Https_request_process_&_output_SSL_certificateIssue_fixed.php
@@ -1,6 +1,7 @@
 <?php
 //phpinfo();
 $ch = curl_init("https://simplenote.com");
+//curl_setopt();
 //curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
 $result = curl_exec($ch);
 $error = curl_error($ch);
